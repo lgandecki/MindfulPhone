4afc5f5cf245fc37e90ed4147589d0a2e4bf3c2f
Lukasz Gandecki <lgandecki@css.edu>
2026-02-12 14:11:19 +0100

    Initial Commit

---
+++ MindfulPhoneTests/MindfulPhoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulPhoneTests.swift
+//  MindfulPhoneTests
+//
+//  Created by Lukasz Gandecki on 12/02/2026.
+//
+
+import Testing
+@testable import MindfulPhone
+
+struct MindfulPhoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
